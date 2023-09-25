1d1f7467746954e26f0136140d36e7bd3cfd89af
Abdul Rehman <mabdul6492@gmail.com>
2023-09-25 23:19:38 +0530

    Genre skeleton

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,26 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  SkeletonText,
+  Text,
+} from "@chakra-ui/react";
 import useGenre from "../hooks/useGenre";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenre();
+  const { data, isLoading } = useGenre();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   return (
     <List>
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <ListItem key={skeleton} paddingY="5px">
+            <Skeleton height="50px" borderRadius={5} />
+          </ListItem>
+        ))}
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
